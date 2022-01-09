84313d466ee55b2e093623850f1065caa211a4aa
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-01-09 18:06:17 -0500

    returns novel information by genre and author id

---
+++ controllers/novels.js
@@ -0,0 +1,16 @@
+const models = require('../models')
+
+
+const getAllNovels = async (request, response) => {
+
+}
+
+const getNovelById = async (request, response) => {
+
+
+}
+
+module.exports = {
+  getAllNovels,
+  getNovelById
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { getAllAuthors, getAllNovelInfoByAuthoId } = require('./controllers/authors')
+const { getAllGenres, getAllNovelInfoByGenreId } = require('./controllers/genres')
+const { getAllNovels, getNovelById } = require('./controllers/novels')
+
+const app = express()
+
+app.get('/authors', getAllAuthors)
+app.get('/authors/:id', getAllNovelInfoByAuthoId)
+
+app.get('/genres', getAllGenres)
+app.get('/genres/:id', getAllNovelInfoByGenreId)
+
+app.get('/novels', getAllNovels)
+app.get('novels/:id', getNovelById)
+
+
+app.listen(1377, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 1377...')
+})
