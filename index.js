f4838be689303326bb8f2e021d54805f72b7b77e
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-01-09 18:16:57 -0500

    final commit

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ const app = express()
 
 app.get('/authors', getAllAuthors)
 app.get('/authors/:id', getAllNovelInfoByAuthoId)
-
 app.get('/genres', getAllGenres)
 app.get('/genres/:id', getAllNovelInfoByGenreId)
-
 app.get('/novels', getAllNovels)
 app.get('/novels/:id', getAllNovelInforByNovelId)
 
-
-app.listen(1377, () => {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port 1377...')
-})
+app.listen(1377)
